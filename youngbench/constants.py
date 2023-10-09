ee4a4ea7a84a95c12be573dc886018d500ade26f
Jason-Young-AI <AI.Jason.Young@outlook.com>
2023-10-09 13:55:18 +0800

    For later use.

---
+++ youngbench/constants.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) Jason Young (杨郑鑫).
+#
+# E-Mail: <AI.Jason.Young@outlook.com>
+# 2023-09-10 14:58
+#
+# This source code is licensed under the Apache-2.0 license found in the
+# LICENSE file in the root directory of this source tree.
+
+
+import enum
+
+
+class Dataset(enum.Enum):
+    PATH = ""
