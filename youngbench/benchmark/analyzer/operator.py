4bdbb13bf64e8634b3c1419b5badef725d7e2604
AI-Jason-Young <AI.Jason.Young@outlook.com>
2023-11-09 00:51:57 +0800

    Add methods of get operator statistics

--- youngbench/benchmark/analyzer/operator.py
+++ youngbench/benchmark/analyzer/operator.py
@@ -10,42 +10,64 @@
 # LICENSE file in the root directory of this source tree.
 
 
-import onnx
+from typing import Set, List, Dict, Union, Tuple
 
-from typing import Set, Dict, Tuple
-
-from youngbench.dataset.modules import Dataset
-from youngbench.constants import ONNX
-
-
-def get_operator_statistics(dataset: Dataset) -> Dict[Tuple[str, str], int]:
-    # All Op and Each Op Num
-    operator_statistics = dict()
-    for instance_identifier, instance in dataset.instances.items():
-        for nn_node_id, nn_node in instance.network.nn_nodes.items():
-            statistics = operator_statistics.get((nn_node.type, nn_node.domain), dict(number=0, nodes=set()))
-            statistics['number'] += 1
-            statistics['nodes'].add(nn_node)
-            node = network.prototype.get_node(node_id)
-            op = (node['op_type'], node['op_domain'])
-            op_num = dataset_ops.get(op, 0)
-            dataset_ops[op] = op_num + 1
-
-    return dataset_ops
+from youngbench.benchmark.analyzer.network import get_networks, get_networks_have_model, get_networks_with_subnetworks
+from youngbench.dataset.modules import Dataset, Prototype
+from youngbench.constants import ONNXOperandType
 
 
-def get_op_stats(dataset: Dataset, op_type: str, op_domain: str) -> Dict:
-    # Op Num in Each Network
-    op = (op_type, op_domain)
-    op_per_network = dict()
-    for network in dataset.networks:
-        network_id = network.identifier
-        num = op_per_network.get(network_id, 0)
-        for node_id in network.prototype.node_ids:
-            node = network.prototype.get_node(node_id)
-            if op == (node['op_type'], node['op_domain']):
-                num += 1
+def get_opstats_of_prototype(prototype: Prototype) -> Dict[Tuple[str, str], Dict[str, Union[int, bool]]]:
+    # dict{
+    #   tuple(op_type, op_domain): dict{num: int, cus: bool}
+    # }
+    opstats = dict()
+    for node in prototype.nn_graph.nodes.values():
+        op = (node['type'], node['domain'])
+        opstat = opstats.get(op, dict(num=0, cus=False))
+        opstat['num'] += 1
+        opstat['cus'] |= bool(node['is_custom'])
+        opstats[op] = opstat
 
-        op_per_network[network_id] = num
+    return opstats
 
-    return op_per_network
+
+def get_opstats_of_dataset(dataset: Dataset, count_model: bool = True) -> Dict[Tuple[str, str], Dict[str, Union[int, bool]]]:
+    # dict{
+    #   tuple(op_type, op_domain): dict{num: int, cus: bool}
+    # }
+    all_networks = get_networks(dataset)
+    opstats = dict()
+    for network in all_networks.values():
+        opstats_of_net = get_opstats_of_prototype(network)
+        for op, opstat_of_pt in opstats_of_net.items():
+            opstat = opstats.get(op, dict(num=0, cus=False))
+            opstat['num'] += opstat_of_pt['num'] * (len(network.models) if count_model else 1)
+            opstat['cus'] |= opstat_of_pt['cus']
+            opstats[op] = opstat
+
+    return opstats
+
+
+def get_opstats_per_model(dataset: Dataset) -> Dict[str, Dict[Tuple[str, str], Dict[str, Union[int, bool]]]]:
+    # dict{
+    #   model_identifier: dict{
+    #     tuple(op_type, op_domain): dict{num: int, cus: bool}
+    #   }
+    # }
+    networks_have_model = get_networks_have_model(dataset)
+    networks_with_subnetworks = get_networks_with_subnetworks(dataset)
+    opstats = dict()
+    for network_id, subnetworks in networks_with_subnetworks.items():
+        opstats_of_net = get_opstats_of_prototype(networks_have_model[network_id])
+        for subnetwork in subnetworks:
+            opstats_of_subnet = get_opstats_of_prototype(subnetwork)
+            for op, opstat_of_subnet in opstats_of_subnet.items():
+                opstat_of_net = opstats_of_net.get(op, dict(num=0, cus=False))
+                opstat_of_net['num'] += opstat_of_subnet['num']
+                opstat_of_net['cus'] |= opstat_of_subnet['cus']
+                opstats_of_net[op] = opstat_of_net
+
+        opstats[network_id] = opstats_of_net
+
+    return opstats
