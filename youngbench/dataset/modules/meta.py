c3d2463f345b1f9dd3b0573c1376a1e9e603baef
AI-Jason-Young <AI.Jason.Young@outlook.com>
2023-12-23 11:57:53 +0800

    Update

--- youngbench/dataset/modules/meta.py
+++ youngbench/dataset/modules/meta.py
@@ -12,20 +12,20 @@
 
 import semantic_version
 
-from typing import Dict
+from typing import Dict, Callable, Optional
 
-from youngbench.dataset.utils.io import hash_string
+from youngbench.dataset.utils.io import load_json, save_json
 
 
 class Meta(object):
     def __init__(
             self,
-            private: bool = False,
+            fresh_checker: Callable[[], bool],
             release: bool = False,
+            release_version: Optional[str] = None,
             retired: bool = False,
-            release_version: str = None,
-            retired_version: str = None,
-            **metrics
+            retired_version: Optional[str] = None,
+            private: bool = False,
     ) -> None:
         if release:
             assert semantic_version.validate(release_version), f'The release version provided must follow the SemVer 2.0.0 Specification.'
@@ -40,74 +40,139 @@ class Meta(object):
         else:
             retired_version = None
 
-        self._private = private
-        self._release = release
-        self._retired = retired
-        self._release_version = release_version
-        self._retired_version = retired_version
-        self._metrics = metrics
+        self._fresh_checker = fresh_checker
 
-    @property
-    def private(self) -> bool:
-        return self._private
+        self._release = release
+        self._release_version = release_version
+        self._retired = retired
+        self._retired_version = retired_version
+
+        self._private = private
+
+        self._legacy = False
+        return
 
     @property
     def release(self) -> bool:
         return self._release
 
     @property
+    def release_version(self) -> Optional[semantic_version.Version]:
+        return self._release_version
+
+    @property
     def retired(self) -> bool:
         return self._retired
 
     @property
-    def release_version(self) -> semantic_version.Version:
-        return self._release_version
-
-    @property
-    def retired_version(self) -> semantic_version.Version:
+    def retired_version(self) -> Optional[semantic_version.Version]:
         return self._retired_version
 
     @property
-    def metrics(self) -> Dict:
-        return self._metrics
+    def private(self) -> bool:
+        return self._private
 
-    @property
-    def dict(self) -> Dict:
-        return dict(
-            private = self.private,
-            release = self.release,
-            retired = self.retired,
-            release_version = str(self.release_version),
-            retired_version = str(self.retired_version),
-            metrics = self.metrics
-        )
+    def load(self, meta_filepath) -> None:
+        info: Dict = load_json(meta_filepath)
 
-    @property
-    def identifier(self) -> str:
-        flags = self.dict
-        flags.pop('metrics')
-        return hash_string(str(tuple(sorted(flags.items()))))
+        if info.get('release', False):
+            self._release = True
+            self._release_version = semantic_version.Version(info['release_version'])
+        if info.get('retired', False):
+            self._retired = True
+            self._retired_version = semantic_version.Version(info['retired_version'])
+        if info.get('private', False):
+            self._private = True
 
-    @property
-    def on_the_fly(self) -> str:
-        return ((not self.release) and (not self.retired))
+        return
+
+    def save(self, meta_filepath) -> None:
+        info: Dict = dict()
+
+        if self._release:
+            info.update(release=True)
+            info.update(release_version=str(self._release_version))
+        if self._retired:
+            info.update(retired=True)
+            info.update(retired_version=str(self._retired_version))
+        if self._private:
+            info.update(private=True)
+
+        save_json(info, meta_filepath)
+        return
 
     def set_release(self, release_version: semantic_version.Version) -> None:
+        assert not self.is_fresh, f'Object is fresh.'
         assert isinstance(release_version, semantic_version.Version), f'The release version provided must follow the SemVer 2.0.0 Specification.'
         assert not self.release, f'Object already has been released!'
 
         self._release = True
         self._release_version = release_version
+        return
 
     def set_retired(self, retired_version: semantic_version.Version) -> None:
+        assert not self.is_fresh, f'Object is fresh.'
         assert isinstance(retired_version, semantic_version.Version), f'The retired version provided must follow the SemVer 2.0.0 Specification.'
         assert not self.retired, f'Object already has been retired!'
         assert self.release, f'Can not retire a Object that has not been released!'
 
-        assert self.release_version < retired_version, (
+        assert self._release_version < retired_version, (
             f'Invalid retired version, it should be larger than the release version:\n'
             f'Release={self.release_version}\n'
             f'Retired={retired_version}'
         )
         self._retired = True
         self._retired_version = retired_version
+        return
+
+    def check(self) -> int:
+        return self.status # This operation can check whether the status is valid.
+
+    @property
+    def status(self) -> int:
+        status = (self._fresh_checker() << 2) + (self._release << 1) + (self._retired or self._legacy)
+        # 0B100 -> It has never been added before.
+        # 0B01X
+        # 0B010 -> Release (New)
+        # 0B011 -> Retired (Old)
+        # 0B00X
+        # 0B000 -> New
+        # 0B001 -> Old
+        assert 0B000 <= status and status <= 0B100, f'Invalid status code: {status}.'
+        return status
+
+    @property
+    def is_fresh(self) -> bool:
+        return self.status == 0B100
+
+    @property
+    def is_release(self) -> bool:
+        return (self.status & 0B111) == 0B010
+
+    @property
+    def is_retired(self) -> bool:
+        return (self.status & 0B111) == 0B011
+
+    @property
+    def is_internal(self) -> bool:
+        return (self.status & 0B110) == 0B010
+
+    @property
+    def is_external(self) -> bool:
+        return (self.status & 0B110) == 0B000
+
+    @property
+    def is_new(self) -> bool:
+        return (self.status & 0B101) == 0B000
+
+    @property
+    def is_old(self) -> bool:
+        return (self.status & 0B101) == 0B001
+
+    def set_new(self) -> None:
+        self._legacy = False
+        return
+
+    def set_old(self) -> None:
+        self._legacy = True
+        return
