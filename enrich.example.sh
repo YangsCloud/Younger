78fc91fa2dd59d7b9c64d6b9df72a513dda39dd1
Jason-Young-AI <AI.Jason.Young@outlook.com>
2024-03-12 01:44:33 +0800

    Update

---
+++ enrich.example.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# Copyright (c) Jason Young (杨郑鑫).
+#
+# E-Mail: <AI.Jason.Young@outlook.com>
+# 2024-03-12 00:41
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+TOKEN="SECRET_TOKEN"
+
+python -m youngbench.dataset.construct.enrich_dataset_using_hf --logging-path /home/jason/0.Research/YoungBench/enrich.log --tag timm --token ${TOKEN};
+python -m youngbench.dataset.construct.enrich_dataset_using_hf --logging-path /home/jason/0.Research/YoungBench/enrich.log --tag sentence-transformers --token ${TOKEN};
+python -m youngbench.dataset.construct.enrich_dataset_using_hf --logging-path /home/jason/0.Research/YoungBench/enrich.log --tag onnx --token ${TOKEN};
+python -m youngbench.dataset.construct.enrich_dataset_using_hf --logging-path /home/jason/0.Research/YoungBench/enrich.log --tag diffusers --token ${TOKEN};
+python -m youngbench.dataset.construct.enrich_dataset_using_hf --logging-path /home/jason/0.Research/YoungBench/enrich.log --tag transformers --token ${TOKEN};
