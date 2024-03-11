7aa20af59eaf1fd7feabc41161a7af5344340c79
Jason-Young-AI <AI.Jason.Young@outlook.com>
2024-03-12 02:48:06 +0800

    Update

--- enrich.example.sh
+++ enrich.example.sh
@@ -10,8 +10,8 @@
 
 TOKEN="SECRET_TOKEN"
 
-python -m youngbench.dataset.construct.enrich_dataset_using_hf --logging-path /home/jason/0.Research/YoungBench/enrich.log --tag timm --token ${TOKEN};
-python -m youngbench.dataset.construct.enrich_dataset_using_hf --logging-path /home/jason/0.Research/YoungBench/enrich.log --tag sentence-transformers --token ${TOKEN};
-python -m youngbench.dataset.construct.enrich_dataset_using_hf --logging-path /home/jason/0.Research/YoungBench/enrich.log --tag onnx --token ${TOKEN};
-python -m youngbench.dataset.construct.enrich_dataset_using_hf --logging-path /home/jason/0.Research/YoungBench/enrich.log --tag diffusers --token ${TOKEN};
-python -m youngbench.dataset.construct.enrich_dataset_using_hf --logging-path /home/jason/0.Research/YoungBench/enrich.log --tag transformers --token ${TOKEN};
+python -m youngbench.dataset.construct.enrich_dataset_using_hf --logging-path /home/jason/0.Research/YoungBench/enrich.log --number 50 --report 200 --tag timm --token ${TOKEN};
+python -m youngbench.dataset.construct.enrich_dataset_using_hf --logging-path /home/jason/0.Research/YoungBench/enrich.log --number 50 --report 200 --tag sentence-transformers --token ${TOKEN};
+python -m youngbench.dataset.construct.enrich_dataset_using_hf --logging-path /home/jason/0.Research/YoungBench/enrich.log --number 50 --report 200 --tag onnx --token ${TOKEN};
+python -m youngbench.dataset.construct.enrich_dataset_using_hf --logging-path /home/jason/0.Research/YoungBench/enrich.log --number 50 --report 200 --tag diffusers --token ${TOKEN};
+python -m youngbench.dataset.construct.enrich_dataset_using_hf --logging-path /home/jason/0.Research/YoungBench/enrich.log --number 50 --report 200 --tag transformers --token ${TOKEN};
