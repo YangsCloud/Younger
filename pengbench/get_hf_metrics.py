dc6e1071851a9249c8c9ec85332eb57c74d588b1
Zrsion <1079451538@qq.com>
2024-04-07 18:19:32 +0800

    update some methods

--- pengbench/get_hf_metrics.py
+++ pengbench/get_hf_metrics.py
@@ -2,17 +2,20 @@ from huggingface_hub import list_metrics
 from huggingface_hub.repocard_data import model_index_to_eval_results
 import pathlib
 import json
+from tqdm import tqdm
 from huggingface_hub import utils, HfFileSystem, ModelCard, ModelCardData, get_hf_file_metadata, hf_hub_url, scan_cache_dir
 from younger.datasets.constructors.huggingface.utils import extract_candidate_metrics_from_readme,get_huggingface_model_readmes
 
 
-model_ids = pathlib.Path("/Users/zrsion/YoungBench/pengbench/20K-Neat.json")
+model_ids = pathlib.Path("/Users/zrsion/YoungBench/pengbench/Small-Neat.json")
 save_dir = pathlib.Path("/Users/zrsion/YoungBench/pengbench")
+
 
 def get_huggingface_metrics(save_dirpath: pathlib.Path):
     metrics = list_metrics()
     ids = [metric.id for metric in metrics]
     return ids
+
 
 def get_eval_results_metric_name(eval_results, hf_metrics_metrics_name: list[str]):
     for eval_result in eval_results:
@@ -22,6 +25,7 @@ def get_eval_results_metric_name(eval_results, hf_metrics_metrics_name: list[str
             hf_metrics_metrics_name.append(metric)
             print(f'new metric name: {metric}')
 
+
 def get_eval_results_metric_type(eval_results, hf_metrics_metrics_type: list[str]):
     for eval_result in eval_results:
         metric = eval_result.metric_type
@@ -29,7 +33,7 @@ def get_eval_results_metric_type(eval_results, hf_metrics_metrics_type: list[str
         if metric not in hf_metrics_metrics_type:
             hf_metrics_metrics_type.append(metric)
             print(f'new metric type: {metric}')
-    
+
 
 def get_metrics(card_data, hf_metrics_metrics: list[str]):
     eval_metrics = card_data.metrics
@@ -40,30 +44,29 @@ def get_metrics(card_data, hf_metrics_metrics: list[str]):
         if metric not in hf_metrics_metrics:
             hf_metrics_metrics.append(metric)
             print(f'new metric: {metric}')
-    
+
 
 if __name__ == '__main__':
 
     hf_metrics_metrics = list()
     hf_metrics_metrics_name = list()
     hf_metrics_metrics_type = list()
-    
+    combined_name_type_list = list()
+    name_and_type = {}
 
-    with open('20K-Neat.json', 'r') as file:
+    with open(model_ids, 'r') as file:
         data = json.load(file)
 
     readmes = get_huggingface_model_readmes(data)
-    for readme in readmes:
+    for readme in tqdm(readmes):
         candidate_metrics = extract_candidate_metrics_from_readme(readme)
         card = ModelCard(readme, ignore_metadata_errors=True)
         card_data: ModelCardData = card.data
         eval_results = card_data.eval_results
-        
+
         get_eval_results_metric_name(eval_results, hf_metrics_metrics_name)
-        get_eval_results_metric_type(eval_results,hf_metrics_metrics_type)
+        get_eval_results_metric_type(eval_results, hf_metrics_metrics_type)
         get_metrics(card_data, hf_metrics_metrics)
-
-
 
     with open(save_dir.joinpath('hf_metrics_metrics_type.json'), 'w') as f:
         json.dump(hf_metrics_metrics_type, f, indent=4)
@@ -71,13 +74,8 @@ if __name__ == '__main__':
     with open(save_dir.joinpath('hf_metrics_metrics.json'), 'w') as f:
         json.dump(hf_metrics_metrics, f, indent=4)
 
-
     with open(save_dir.joinpath('hf_metrics_metrics_name.json'), 'w') as f:
         json.dump(hf_metrics_metrics_name, f, indent=4)
 
-
-
-
-
-
-
+    # with open(save_dir.joinpath('combined_name_type_list.json'), 'w') as f:
+    #     json.dump(combined_name_type_list, f, indent=4)
