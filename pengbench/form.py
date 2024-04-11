813a352a6bdc2d5aaecc93224482646c0cc13361
Jason Young <AI.Jason.Young@outlook.com>
2024-04-11 20:31:07 +0800

    Delete pengbench/form.py

--- pengbench/form.py
+++
@@ -1,8 +0,0 @@
-import json
-
-
-if __name__ == '__main__':
-    with open("/Users/zrsion/YoungBench/pengbench/template.json",'w') as f:
-        with open("/Users/zrsion/YoungBench/pengbench/example.json", 'r') as input_file:
-            data = json.load(input_file)
-        json.dump(data,f, indent=4)
