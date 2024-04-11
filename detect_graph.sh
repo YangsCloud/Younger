0156c250a9abe9604e54ef9fa8974fc340250265
Jason Young <AI.Jason.Young@outlook.com>
2024-04-11 20:28:12 +0800

    Delete detect_graph.sh

--- detect_graph.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-INSTANCE_DIR="/Users/zrsion/instance-task"
-SAVE_DIR="/Users/zrsion/instance-task"
-python -m pengbench.detect_graph --instance-task-dir ${INSTANCE_DIR} --save-dir ${SAVE_DIR}
