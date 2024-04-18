7057161d13fdd4aa02c8e22c13326a6056375011
Zrsion <1079451538@qq.com>
2024-04-18 09:49:57 +0800

    update

--- younger/datasets/constructors/official/statistics.py
+++ younger/datasets/constructors/official/statistics.py
@@ -17,13 +17,13 @@ import networkx
 from typing import Dict 
 import itertools
 
-instance_dir = pathlib.Path('/Users/zrsion/instances-0.0.1')
-save_dir = pathlib.Path('/Users/zrsion/instances_statistics')
 
-combination_set = set()
+instance_dir = pathlib.Path('/Users/zrsion/instances-0.0.1') # test
+save_dir = pathlib.Path('/Users/zrsion/instances_statistics') # test
+
             
 def get_operators(nodes: networkx.DiGraph.nodes) -> Dict[str, int]:
-    operators_dict = {}
+    operators_dict = dict()
     for node in nodes(data = 'operator'):
         if not node[1]: # node : e.g. ('589', None), node[1]==None , This will skip nodes with node_type == 'input' | 'output' | 'constant' | 'outer'
             continue
@@ -40,29 +40,18 @@ def get_operators(nodes: networkx.DiGraph.nodes) -> Dict[str, int]:
 def generater_statistics(instance_dir: pathlib.Path, save_dir: pathlib.Path,
                          tasks: list[str], datasets: list[str], splits: list[str], metrics: list[str]):
     
-    combinations = []
-    task_iter = tasks if tasks != [] else ['']
-    dataset_iter = datasets if datasets != [] else ['']
-    split_iter = splits if splits != [] else ['']
-    metric_iter = metrics if metrics != [] else ['']
+    combinations = list()
+    task_iter = tasks if tasks else ['']
+    dataset_iter = datasets if datasets else ['']
+    split_iter = splits if splits else ['']
+    metric_iter = metrics if metrics else ['']
 
     for task, dataset, split, metric in itertools.product(task_iter, dataset_iter, split_iter, metric_iter):
         combination = (task,) + (dataset,) + (split,) + (metric,)
         combination = tuple(item for item in combination if item)
         combinations.append(str(combination))
     
-    save_filename = "sta"
-    if tasks != []:
-        save_filename += "_" + "tasks" 
-    if datasets != []:
-        save_filename += "_" + "datasets" 
-    if splits != []:
-        save_filename += "_" + "splits"
-    if metrics != []:
-        save_filename += "_" + "metrics" 
-    save_filename = save_filename.strip("_") + ".json"
-    
-    final_dic = {}
+    final_dic = dict()
     cnt = 0 # test
     
     instance_paths = list(instance_dir.iterdir())
@@ -75,33 +64,42 @@ def generater_statistics(instance_dir: pathlib.Path, save_dir: pathlib.Path,
         
         if cnt > 15: break # test
         
-        
         if instance.labels:
             labels = instance.labels['labels']
             
             if labels:
                 for label in labels:
-                    task = label['task'] if tasks != [] else ''
-                    dataset = label['dataset'][0] if datasets != [] else ''
-                    split = label['dataset'][1] if splits != [] else ''
-                    metric = label['metric'][0] if metrics != [] else ''
+                    task = label['task'] if tasks else ''
+                    dataset = label['dataset'][0] if datasets else ''
+                    split = label['dataset'][1] if splits else ''
+                    metric = label['metric'][0] if metrics else ''
                     value = label['metric'][1]
                     
                     combination = (task,) + (dataset,) + (split,) + (metric,)
-                    combination = tuple(item for item in combination if item) 
-                    combination = str(combination)
+                    combination = str(tuple(item for item in combination if item))
                     
                     if combination in combinations:
-                        dic_to_add = {}
-                        dic_to_add[model_id] = []
+                        dic_to_add = dict()
+                        dic_to_add[model_id] = list()
                         dic_to_add[model_id].extend([value, model_operators])
                         if combination not in final_dic:
-                            final_dic[combination] = []
+                            final_dic[combination] = list()
                         final_dic[combination].append(dic_to_add)
                         cnt += 1 # test
                         break
     
-    with open(save_dir.joinpath(save_filename), 'w') as f:
+    save_file_name_suffix = "sta"
+    if tasks:
+        save_file_name_suffix += "_" + "tasks" 
+    if datasets:
+        save_file_name_suffix += "_" + "datasets" 
+    if splits:
+        save_file_name_suffix += "_" + "splits"
+    if metrics:
+        save_file_name_suffix += "_" + "metrics" 
+    save_file_name_suffix = save_file_name_suffix.strip("_") + ".json"
+    
+    with open(save_dir.joinpath(save_file_name_suffix), 'w') as f:
         json.dump(final_dic, f, indent=4)
     
                 
