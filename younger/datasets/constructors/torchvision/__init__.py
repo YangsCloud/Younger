80dc9f7d60fa5dec420a2629cf88ae20d51c260a
Jason-Young-AI <AI.Jason.Young@outlook.com>
2024-05-12 13:08:08 +0800

    Add TorchVision Construction

---
+++ younger/datasets/constructors/torchvision/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) Jason Young (杨郑鑫).
+#
+# E-Mail: <AI.Jason.Young@outlook.com>
+# 2024-05-12 08:55
+#
+# This source code is licensed under the Apache-2.0 license found in the
+# LICENSE file in the root directory of this source tree.
