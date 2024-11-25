3baf453bafc987806d2bd3272c6a3cda1277c5e2
AI-Jason-Young <AI.Jason.Young@outlook.com>
2024-11-25 15:59:42 +0800

    Update

--- younger/__init__.py
+++ younger/__init__.py
@@ -8,3 +8,8 @@
 #
 # This source code is licensed under the Apache-2.0 license found in the
 # LICENSE file in the root directory of this source tree.
+
+
+import importlib.metadata
+
+__version__ = importlib.metadata.version("younger")
