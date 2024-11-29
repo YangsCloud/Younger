c38862237411720d09a387883603f93124c50a7d
AI-Jason-Young <AI.Jason.Young@outlook.com>
2024-11-29 22:30:55 +0800

    Versioning

--- younger/__init__.py
+++ younger/__init__.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-#
-# Copyright (c) Jason Young (杨郑鑫).
-#
-# E-Mail: <AI.Jason.Young@outlook.com>
-# 2023-09-10 15:02
-#
-# This source code is licensed under the Apache-2.0 license found in the
+# -*- encoding=utf8 -*-
+
+########################################################################
+# Created time: 2024-08-27 18:03:43
+# Author: Jason Young (杨郑鑫).
+# E-Mail: AI.Jason.Young@outlook.com
+# Last Modified by: Jason Young (杨郑鑫)
+# Last Modified time: 2024-11-28 11:17:36
+# Copyright (c) 2024 Yangs.AI
+# 
+# This source code is licensed under the Apache License 2.0 found in the
 # LICENSE file in the root directory of this source tree.
+########################################################################
+
+
+from . import commons
 
 
 import importlib.metadata
